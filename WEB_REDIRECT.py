89382477f821d12da1ffaeca85108ffe598638ad
ManuAP <127395100+ManuAP@users.noreply.github.com>
2023-03-09 12:05:26 +0100

    WEB_REDIRECT.py
    Este script es un servidor web muy simple que redirige cualquier solicitud a una URL específica. Cuando alguien accede a la dirección del servidor web, el servidor devuelve una respuesta HTTP 302 (redirección temporal) que indica que el recurso solicitado se ha movido temporalmente a una nueva URL. La nueva URL se especifica en la línea de comandos cuando se ejecuta el script como argumento. Por lo tanto, cualquier solicitud recibida por este servidor se redirige automáticamente a la URL especificada como argumento en la línea de comandos. El servidor se ejecuta en el puerto 80 y en cualquier dirección IP disponible (0.0.0.0).

---
+++ WEB_REDIRECT.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class Redirect(BaseHTTPRequestHandler):
+  def do_GET(self):
+      self.send_response(302)
+      self.send_header('Location', sys.argv[1])
+      self.end_headers()
+
+HTTPServer(("0.0.0.0", 80), Redirect).serve_forever()
